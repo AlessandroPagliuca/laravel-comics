147014df32bb67df6cfac5c21e46be368d879cdc
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-05-22 23:58:38 +0200

    Upgraded route comicBooks

--- routes/web.php
+++ routes/web.php
@@ -96,7 +96,79 @@ Route::get('/', function () {
     return view('home', compact('data', 'products', 'footerLinks'));
 })->name('home');
 
-Route::get('/products.comicBooks', function () {
+Route::get('/products.comicBooks/{id}', function ($id) {
+    $data = [
+        'character',
+        'comics',
+        'movies',
+        'tv',
+        'games',
+        'collectibles',
+        'videos',
+        'fans',
+        'news',
+        'shop',
 
-    return view('products.comicBooks');
+    ];
+
+    $products = [
+        'products' => config('comics')
+
+    ];
+    $product = $products[$id] ?? null;
+    //dd(config('comics'));
+
+    $footerLinks = [
+        'dc_comics' => [
+            'title' => 'dc comics',
+            'links' => [
+                'characters',
+                'comics',
+                'movies',
+                'tv',
+                'games',
+                'videos',
+                'news',
+            ]
+        ],
+
+        'dc' => [
+            'title' => 'dc',
+            'links' => [
+                'terms of use',
+                'privacy policy (new)',
+                'ad choices',
+                'advertising',
+                'jobs',
+                'subscriptions',
+                'talent workshops',
+                'cpsc certificates',
+                'ratings',
+                'shop help',
+                'contact us',
+
+            ]
+        ],
+
+        'sites' => [
+            'title' => 'sites',
+            'links' => [
+                'dc',
+                'mad magazine',
+                'dc kids',
+                'dc universe',
+                'dc power visa',
+            ]
+        ],
+
+        'shop' => [
+            'title' => 'shop',
+            'links' => [
+                'shop dc',
+                'shop dc collectibles',
+            ]
+        ]
+
+    ];
+    return view('products.comicBooks', compact('data', 'products', 'footerLinks'));
 })->name('products.comicBooks');
