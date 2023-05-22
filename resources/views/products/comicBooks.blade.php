33c482114409d9e599ae9d73979188a42a2dc7aa
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-05-22 23:58:08 +0200

    Upgraded single card description

---
+++ resources/views/products/comicBooks.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>fumetti</h1>
+
+
+    <div class="col-12 col-sm-6 col-md-4 col-lg-2 d-flex justify-content-center">
+
+        <div class="card m-2" style="width: 14rem; height: 14rem;">
+            <img class="card-img-top" src="{{ $product['thumb'] }}" alt="{{ $product['series'] }}">
+            <div class="card-body">
+                <h5 class="card-title">{{ $product['title'] }}</h5>
+                <p class="card-text">{{ $product['description'] }}</p>
+                <p class="card-text"><strong>Price:</strong> {{ $product['price'] }}</p>
+                <p class="card-text"><strong>Series:</strong> {{ $product['series'] }}</p>
+                <p class="card-text"><strong>Sale Date:</strong> {{ $product['sale_date'] }}</p>
+                <p class="card-text"><strong>Type:</strong> {{ $product['type'] }}</p>
+                <p class="card-text"><strong>Artists:</strong> {{ implode(', ', $product['artists']) }}</p>
+                <p class="card-text"><strong>Writers:</strong> {{ implode(', ', $product['writers']) }}</p>
+            </div>
+        </div>
+
+    </div>
+@endsection
