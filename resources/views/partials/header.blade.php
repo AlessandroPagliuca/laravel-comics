7cef925a05b9bf1887670ebc26d77b4bd9672e8c
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-05-22 22:59:59 +0200

    Upgraded header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+<header>
+    <!-- dc power visa additional dc sites -->
+    <div class="bg-primary box-visa">
+        <div class="container d-flex justify-content-end align-items-center">
+            <div class="text-uppercase text-white px-4">dc power visa</div>
+            <div class="text-uppercase text-white px-4">additional dc sites</div>
+        </div>
+    </div>
+    <!-- navbar -->
+    <nav class="py-4">
+        <div class="container d-flex justify-content-center align-items-center">
+            <div class="box-logo">
+                <img src="{{ asset('resources/img/dc-logo.png') }}" alt="dc-comics">
+            </div>
+
+            <ul class="d-flex">
+                @foreach ($data as $value)
+                    <li class="text-uppercase fw-semibold">{{ $value }}</li>
+                @endforeach
+            </ul>
+        </div>
+    </nav>
+</header>
